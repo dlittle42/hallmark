2cde178a3bcb26545469eab3419725b8dcb7cada
derek little <dlittle@WS171-MBAir.local>
2016-11-03 10:52:00 -0400

    toBlob polyfill

--- src/App.vue
+++ src/App.vue
@@ -234,7 +234,7 @@ export default {
      document.getElementById("thanks-close").addEventListener('click', this.closeHelp, false);
 
      
-
+     this.checkToBlob();
       
       var parser = new UAParser();
 
@@ -263,6 +263,25 @@ export default {
       getAlert() {
         alert('function call');
       },
+      checkToBlob: function(){
+        if (!HTMLCanvasElement.prototype.toBlob) {
+         Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+          value: function (callback, type, quality) {
+
+            var binStr = atob( this.toDataURL(type, quality).split(',')[1] ),
+                len = binStr.length,
+                arr = new Uint8Array(len);
+
+            for (var i=0; i<len; i++ ) {
+             arr[i] = binStr.charCodeAt(i);
+            }
+
+            callback( new Blob( [arr], {type: type || 'image/png'} ) );
+          }
+         });
+        }
+      },
+
       updateMessage: function(idx){
         this.selectedMsg = idx+1;
       },
